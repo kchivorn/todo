ae21342013b6b309dc1e8b70b53d0791f1f61150
kchivorn <kouch.chivorn@gmail.com>
2017-11-28 14:48:01 +0700

    Add validation to true for column content and deadline of todo_item

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -1,5 +1,7 @@
 class TodoItem < ApplicationRecord
   belongs_to :todo_list
+  validates :content, presence: true
+  validates :deadline, presence: true
 
   def completed?
     completed_at.present?
