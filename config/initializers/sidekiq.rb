0f0054a3541ab487f45bd0400ca1a9c12a4944b4
kchivorn <kouch.chivorn@gmail.com>
2017-12-04 09:55:04 +0700

    update sidekiq config with redis url

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,17 @@
-Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379/5'}
-end
+if Rails.env.production?
+  Sidekiq.configure_server do |config|
+    config.redis = { url: ENV['REDIS_URL'], size: 5 }
+  end
 
-Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost:6379/5'}
+  Sidekiq.configure_client do |config|
+    config.redis = { url: ENV['REDIS_URL'], size: 2 }
+  end
+elsif Rails.env.development?
+  Sidekiq.configure_server do |config|
+    config.redis = { url: 'redis://localhost:6379/5'}
+  end
+
+  Sidekiq.configure_client do |config|
+    config.redis = { url: 'redis://localhost:6379/5'}
+  end
 end
