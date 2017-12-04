facabe06b20ddc550259e8dfca41620d44b90bcc
kchivorn <kouch.chivorn@gmail.com>
2017-12-04 11:30:59 +0700

    update puma

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -7,20 +7,20 @@ if Rails.env.production?
     config.redis = { url: ENV['REDIS_URL'], size: 2 }
   end
 
-  Rails.application.config.after_initialize do
-    Rails.logger.info("DB Connection Pool size for Sidekiq Server before disconnect is: #{ActiveRecord::Base.connection.pool.instance_variable_get('@size')}")
-    ActiveRecord::Base.connection_pool.disconnect!
+  # Rails.application.config.after_initialize do
+  #   Rails.logger.info("DB Connection Pool size for Sidekiq Server before disconnect is: #{ActiveRecord::Base.connection.pool.instance_variable_get('@size')}")
+  #   ActiveRecord::Base.connection_pool.disconnect!
 
-    ActiveSupport.on_load(:active_record) do
-      config = Rails.application.config.database_configuration[Rails.env]
-      config['reaping_frequency'] = ENV['DATABASE_REAP_FREQ'] || 10 # seconds
-      config['pool'] = ENV['WORKER_DB_POOL_SIZE'] || Sidekiq.options[:concurrency]
-      #config['pool'] = 20
-      ActiveRecord::Base.establish_connection(config)
+  #   ActiveSupport.on_load(:active_record) do
+  #     config = Rails.application.config.database_configuration[Rails.env]
+  #     config['reaping_frequency'] = ENV['DATABASE_REAP_FREQ'] || 10 # seconds
+  #     config['pool'] = ENV['WORKER_DB_POOL_SIZE'] || Sidekiq.options[:concurrency]
+  #     #config['pool'] = 20
+  #     ActiveRecord::Base.establish_connection(config)
 
-      Rails.logger.info("DB Connection Pool size for Sidekiq Server is now: #{ActiveRecord::Base.connection.pool.instance_variable_get('@size')}")
-    end
-  end
+  #     Rails.logger.info("DB Connection Pool size for Sidekiq Server is now: #{ActiveRecord::Base.connection.pool.instance_variable_get('@size')}")
+  #   end
+  # end
 elsif Rails.env.development?
   Sidekiq.configure_server do |config|
     config.redis = { url: 'redis://localhost:6379/5'}
