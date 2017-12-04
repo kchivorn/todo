21b489eb162e6d9b912fc62e278ff260053c5373
kchivorn <kouch.chivorn@gmail.com>
2017-12-04 09:30:57 +0700

    add puts to mail sending and nil to memcachedstore

--- app/mailers/users_mailer.rb
+++ app/mailers/users_mailer.rb
@@ -2,6 +2,7 @@ class UsersMailer < ApplicationMailer
   def inform_todolist(user_id, todo_list_id)
     @user = User.find(user_id)
     @todo_list = TodoList.find(todo_list_id)
+    puts "mail is sending now ..."
     mail( to: @user.email,
       subject: @todo_list.title
     ) do |format|
