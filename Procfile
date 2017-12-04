bcc5c4bb19713825763b1a9521644a6780d2926a
kchivorn <kouch.chivorn@gmail.com>
2017-12-04 11:24:16 +0700

    upate procfile and sidekiq.rb

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: bundle exec puma -t 5:5 -p ${PORT:-3000} -e ${RACK_ENV:-development}
-worker: bundle exec sidekiq -e production
+web: bundle exec puma -C config/puma.rb
+worker: bundle exec sidekiq -e production -C config/sidekiq.yml
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -14,8 +14,8 @@ if Rails.env.production?
     ActiveSupport.on_load(:active_record) do
       config = Rails.application.config.database_configuration[Rails.env]
       config['reaping_frequency'] = ENV['DATABASE_REAP_FREQ'] || 10 # seconds
-      # config['pool'] = ENV['WORKER_DB_POOL_SIZE'] || Sidekiq.options[:concurrency]
-      config['pool'] = 20
+      config['pool'] = ENV['WORKER_DB_POOL_SIZE'] || Sidekiq.options[:concurrency]
+      #config['pool'] = 20
       ActiveRecord::Base.establish_connection(config)
 
       Rails.logger.info("DB Connection Pool size for Sidekiq Server is now: #{ActiveRecord::Base.connection.pool.instance_variable_get('@size')}")
