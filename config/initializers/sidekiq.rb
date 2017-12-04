b12c8b08080ef5cfcea31cd33c234cee25de2d26
kchivorn <kouch.chivorn@gmail.com>
2017-12-04 11:13:53 +0700

    update connection pool to 20 in database.yml in production

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -15,7 +15,7 @@ if Rails.env.production?
       config = Rails.application.config.database_configuration[Rails.env]
       config['reaping_frequency'] = ENV['DATABASE_REAP_FREQ'] || 10 # seconds
       # config['pool'] = ENV['WORKER_DB_POOL_SIZE'] || Sidekiq.options[:concurrency]
-      config['pool'] = 16
+      config['pool'] = 20
       ActiveRecord::Base.establish_connection(config)
 
       Rails.logger.info("DB Connection Pool size for Sidekiq Server is now: #{ActiveRecord::Base.connection.pool.instance_variable_get('@size')}")
